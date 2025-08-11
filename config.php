7557cf57c86095170378bf022ae869eadbec5952
Egor <aidinian1998@mail.ru>
2025-08-11 19:40:49 +0700

    Добавил в config url на свой вебхук тестовый

--- config.php
+++ config.php
@@ -1,3 +1,6 @@
 <?php
 // URL входящего вебхука Битрикс24, заканчивается на /rest/...
-define('WEBHOOK_URL', 'https://your-domain.bitrix24.ru/rest/1/your_webhook_code/');
+// define('WEBHOOK_URL', 'https://your-domain.bitrix24.ru/rest/1/your_webhook_code/');
+
+// создал тестово у себя 
+define('WEBHOOK_URL', 'https://b24-p0qysc.bitrix24.ru/rest/1/7hrjc0099g1exm4g/');
